5bcfc9ac55f433f50d644b8a474410a7bace0e61
Eequiis Vásquez <42697474+Eequiis@users.noreply.github.com>
2020-11-13 12:59:59 +0100

    Añadido método Paginas; Texto de DebeBloquearPila obsoleto mejorado

--- Eequiis.Xamarin.Popup/Servicios/ExtensionesPilaPopup.cs
+++ Eequiis.Xamarin.Popup/Servicios/ExtensionesPilaPopup.cs
@@ -26,6 +26,16 @@ namespace Eequiis.Xamarin.Popup.Servicios
 			=> navigation?.PopupStack is IReadOnlyList<PopupPage> pila && pila.Count > 0 ? pila.Last() : null;
 
 		/// <summary>
+		/// <para>Dado un <see cref="IPopupNavigation"/>, obtiene el número de páginas (<see cref="PopupPage"/>) que
+		/// contiene la pila del objeto de navegación.</para>
+		/// <para>Si el objeto de navegación de páginas indicado es nulo, o su pila es nula, se devolverá 0.</para>
+		/// </summary>
+		/// <param name="navigation">Objeto de navegación cuyo número de páginas de popup se va a consultar.</param>
+		/// <returns>El número de páginas del objeto de navegación indicado.</returns>
+		public static int Paginas(this IPopupNavigation navigation)
+			=> navigation?.PopupStack is IReadOnlyList<PopupPage> pila ? pila.Count : 0;
+
+		/// <summary>
 		/// <para>Comprueba si la pila del navegador indicado debería estar bloqueada, es decir, si se debería impedir
 		/// que se pueda desapilar páginas.</para>
 		/// <para>En este sentido, se considerará que un navegador debería bloquear su pila si su cima es un página de
@@ -36,7 +46,7 @@ namespace Eequiis.Xamarin.Popup.Servicios
 		/// <param name="navigation">Navegador de páginas de popup cuya pila se va a comprobar.</param>
 		/// <returns>Un valor booleano que indica si la pila del navegador indicado debería bloquer las operaciones de
 		/// desapilado.</returns>
-		[Obsolete("Sustituir por comprobación de tipos con \"is\".", false)]
+		[Obsolete("Sustituir por comprobación de tipos \"CimaPila() is PopupCarga\".", false)]
 		public static bool DebeBloquearPila(this IPopupNavigation navigation) => navigation.CimaPila() is PopupCarga;
 	}
 }
